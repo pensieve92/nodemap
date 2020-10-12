c242b28b0949fd5db7ffdedcb2d6f1f3414fa7cc
unknown <pensieve92@gmail.com>
2020-10-12 22:33:18 +0900

    add TuiEditor

---
+++ front/components/NoteTabContainer.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+// import QuillEditor from './QuillEditor';
+import TuiEditor from './TuiEditor';
+
+
+
+const NoteTabContiner = ({data}) => {    
+    return (        
+        <div>            
+            {data}
+            <TuiEditor />
+        </div>
+    )
+}
+
+export default NoteTabContiner;
+
