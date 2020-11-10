6417e66bd7e7182e22715b888dffc1a7485ce9ef
unknown <pensieve92@gmail.com>
2020-11-11 00:43:05 +0900

    add loginForm

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Head from 'next/head';
-import { Input, Layout, Menu, Breadcrumb, PageHeader, Tag, Button } from 'antd'; 
+import { Input, Layout, Menu, Breadcrumb, PageHeader, Tag, Button, Divider, Row, Col, Tooltip, Typography } from 'antd'; 
+import { InfoCircleOutlined, UserOutlined } from '@ant-design/icons';
 import styled from 'styled-components';
 
+const { Title, Paragraph, Text, Link } = Typography;
 const ContentWrapper = styled.div`    
-    padding: 0 50px;
+    padding: 0 4rem;
     height: calc(100vh - 10rem);  
 `
 
@@ -35,9 +37,65 @@ const Home = () => {
                         avatar={{ src: '/maessil.png' }}                        
                     />
                     <ContentWrapper>                    
-                        <div className="site-layout-content">Content</div>
+                        {/* <div className="site-layout-content">Content</div> */}
+                        <Divider orientation="left">Hellow MaeSsil</Divider>
+                            <Row justify="center">                                                            
+                                <Col span={16}>
+                                    <div style={{height: '23rem', padding: '0.5rem'}}>
+                                    <Title level={2}>Guidelines and Resources</Title>
+                                        <Paragraph>
+                                        We supply a series of design principles, practical patterns and high quality design resources
+                                        (<Text code>Sketch</Text> and <Text code>Axure</Text>), to help people create their product
+                                        prototypes beautifully and efficiently.
+                                        </Paragraph>
+                                        <Paragraph>
+                                        We supply a series of design principles, practical patterns and high quality design resources
+                                        (<Text code>Sketch</Text> and <Text code>Axure</Text>), to help people create their product
+                                        prototypes beautifully and efficiently.
+                                        </Paragraph>
+                                        <Paragraph>
+                                        We supply a series of design principles, practical patterns and high quality design resources
+                                        (<Text code>Sketch</Text> and <Text code>Axure</Text>), to help people create their product
+                                        prototypes beautifully and efficiently.
+                                        </Paragraph>
+                                        <Paragraph>
+                                            <ul>
+                                                <li>
+                                                <Link href="/docs/spec/proximity">Principles</Link>
+                                                </li>
+                                                <li>
+                                                <Link href="/docs/pattern/navigation">Patterns</Link>
+                                                </li>
+                                                <li>
+                                                <Link href="/docs/resource/download">Resource Download</Link>
+                                                </li>
+                                            </ul>
+                                        </Paragraph>
+                                    </div>
+                                </Col>                                
+                                <Col span={8}>
+                                    <div style={{height: '23rem', backgroundColor:'white', borderRadius:'0.5rem', padding: '0.5rem'}}>
+                                        <Divider orientation="left">ID</Divider>
+                                             <Input
+                                                name='user_id'
+                                                placeholder="Enter your id"
+                                                prefix={<UserOutlined className="site-form-item-icon" />}
+                                                suffix={
+                                                    <Tooltip title="Extra information">
+                                                    <InfoCircleOutlined style={{ color: 'rgba(0,0,0,.45)' }} />
+                                                    </Tooltip>
+                                                }
+                                            />
+                                        <Divider orientation="left">E-MAIL</Divider>
+                                        <Input name='user_email' type="email" placeholder="input email"/>
+                                        <Divider orientation="left">PASSWORD</Divider>
+                                        <Input.Password name='user_password' placeholder="input password" />
+                                        <Button type="primary"  style={{height:'4rem', width:'100%', marginTop:'1rem', fontWeight:'600', fontSize:'1.5rem'}}> Sign up for MaeSsil</Button>
+                                    </div>
+                                </Col>                         
+                            </Row>
                     </ContentWrapper>
-                    <Footer style={{ textAlign: 'center', height: '5rem' }}>Ant Design ©2018 Created by Ant UED</Footer>
+                    <Footer style={{ textAlign: 'center', height: '5rem' }}>maessil.com ©2020 Created by BH JEON</Footer>
                 </Layout>                              
             </div>
         )
